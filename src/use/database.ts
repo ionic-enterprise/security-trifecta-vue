a0a5dba25c0b37984603b25c1dec876a3f630a3b
Ken Sodemann <ken@ionic.io>
2021-11-23 14:43:25 -0600

    feat(notes): work out the CRUD so we can sync WIP - trim

--- src/use/database.ts
+++ src/use/database.ts
@@ -102,13 +102,111 @@ const getTastingNotes = async (user: User): Promise<Array<TastingNote>> => {
   return notes;
 };
 
+const addTastingNote = async (note: TastingNote, user: User): Promise<void> => {
+  if ((await isReady()) && handle) {
+    const n = { ...note };
+    await handle.transaction((tx) => {
+      tx.executeSql(
+        'SELECT COALESCE(MAX(id), 0) + 1 AS newId FROM TastingNotes',
+        [],
+        // tslint:disable-next-line:variable-name
+        (_t: any, r: any) => {
+          n.id = r.rows.item(0).newId;
+          tx.executeSql(
+            'INSERT INTO TastingNotes (id, name, brand, notes, rating, teaCategoryId, userId, syncStatus)' +
+              ' VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [n.id, n.name, n.brand, n.notes, n.rating, n.teaCategoryId, user.id, 'INSERT'],
+            () => {
+              null;
+            }
+          );
+        }
+      );
+    });
+  }
+};
+
+const resetTastingNotes = async (user: User): Promise<void> => {
+  if ((await isReady()) && handle) {
+    await handle.transaction((tx) => {
+      tx.executeSql(
+        "UPDATE TastingNotes SET syncStatus = null WHERE syncStatus = 'UPDATE' AND userId = ?",
+        [user.id],
+        () => {
+          null;
+        }
+      );
+      tx.executeSql(
+        "DELETE FROM TastingNotes WHERE syncStatus in ('DELETE', 'INSERT') AND userId = ?",
+        [user.id],
+        () => {
+          null;
+        }
+      );
+    });
+  }
+};
+
+const deleteTastingNote = async (note: TastingNote, user: User): Promise<void> => {
+  if ((await isReady()) && handle) {
+    await handle.transaction((tx) => {
+      tx.executeSql(
+        "UPDATE TastingNotes SET syncStatus = 'DELETE' WHERE userId = ? AND id = ?",
+        [user.id, note.id],
+        () => {
+          null;
+        }
+      );
+    });
+  }
+};
+
+const params = (length: number): string => {
+  let str = '';
+  for (let i = 0; i < length; i++) {
+    str += `${i ? ', ' : ''}?`;
+  }
+  return str;
+};
+
+const trimTastingNotes = async (idsToKeep: Array<number>, user: User): Promise<void> => {
+  if ((await isReady()) && handle) {
+    await handle.transaction((tx) => {
+      tx.executeSql(
+        `DELETE FROM TastingNotes WHERE userId = ? AND id not in (${params(idsToKeep.length)})`,
+        [user.id, ...idsToKeep],
+        () => {
+          null;
+        }
+      );
+    });
+  }
+};
+
+const updateTastingNote = async (note: TastingNote, user: User): Promise<void> => {
+  if ((await isReady()) && handle) {
+    await handle.transaction((tx) => {
+      tx.executeSql(
+        'UPDATE TastingNotes SET name = ?, brand = ?, notes = ?, rating = ?, teaCategoryId = ?,' +
+          " syncStatus = CASE syncStatus WHEN 'INSERT' then 'INSERT' when 'DELETE' then 'DELETE' else 'UPDATE' END" +
+          ' WHERE syncStatus is NULL AND userId = ? AND id = ?',
+        [note.name, note.brand, note.notes, note.rating, note.teaCategoryId, user.id, note.id],
+        () => {
+          null;
+        }
+      );
+    });
+  }
+};
+
 const mergeTastingNote = async (note: TastingNote, user: User): Promise<void> => {
   if ((await isReady()) && handle) {
     await handle.transaction((tx) => {
       tx.executeSql(
         'INSERT INTO TastingNotes (id, name, brand, notes, rating, teaCategoryId, userId) VALUES (?, ?, ?, ?, ?, ?, ?)' +
           ' ON CONFLICT(id) DO' +
-          ' UPDATE SET name = ?, brand = ?, notes = ?, rating = ?, teaCategoryId = ? where syncStatus is NULL AND id = ?',
+          ' UPDATE SET name = ?, brand = ?, notes = ?, rating = ?, teaCategoryId = ?' +
+          ' WHERE syncStatus is NULL AND userId = ? AND id = ?',
         [
           note.id,
           note.name,
@@ -122,6 +220,7 @@ const mergeTastingNote = async (note: TastingNote, user: User): Promise<void> =>
           note.notes,
           note.rating,
           note.teaCategoryId,
+          user.id,
           note.id,
         ],
         () => {
@@ -150,7 +249,14 @@ const mergeTeaCategory = async (cat: TeaCategory): Promise<void> => {
 
 export default () => ({
   getTastingNotes,
+  mergeTeaCategory,
+
+  addTastingNote,
+  deleteTastingNote,
+  updateTastingNote,
+  resetTastingNotes,
+  trimTastingNotes,
+
   getTeaCategories,
   mergeTastingNote,
-  mergeTeaCategory,
 });
